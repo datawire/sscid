93f96bdb8ef66dcd5fac3df2603119b41b464b96
Philip Lombardi <plombardi@datawire.io>
2017-08-28 15:34:20 -0400

    init commit

---
+++ sscid.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+# Project config
+PROJECT_NAME="datawire/sscid"
+GIT_URL="https://github.com/${PROJECT_NAME}.git"
+
+BRANCH=master
+SCRIPT=test.sh
+
+# SSCID config and setup
+SSCID_WORKSPACE=${HOME}/sscid
+PROJECT_WORKSPACE=${SSCID_WORKSPACE}/${PROJECT_NAME}
+
+mkdir -p ${PROJECT_WORKSPACE}/results
+cd ${PROJECT_WORKSPACE}
+
+if [ -d "./latest" ]; then
+  printf "Run in progress; Not continuing"
+  exit 0
+fi
+
+git clone ${GIT_URL} latest
+
+cd latest
+git checkout ${BRANCH}
+GIT_COMMIT=$(git rev-parse HEAD)
+
+# execute the build script and shit the output to a file
+./${SCRIPT} > sscid.log
+
+echo "$?" > sscid.result
+
+cp -R . ${PROJECT_WORKSPACE}/${GIT_COMMIT}
+rm -rf ${PROJECT_WORKSPACE}/latest
+
+# Upload log to S3
+
+cd ${PROJECT_WORKSPACE}
+aws s3 sync . s3://sscid --exclude "*latest/*" --include "*/sscid.result" --include "*/sscid.log"
+
